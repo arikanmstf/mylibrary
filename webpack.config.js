538217824507d8add41299c7ac2860c54aa7fea9
Mustafa Arikan <arikanmstf@gmail.com>
2017-06-01 23:36:51 +0300

    Environment installed

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+let extractCSS = new ExtractTextPlugin('style.css');
+
 module.exports = {
   entry: [
-    './src/index.js'
+    './src/scripts/index.js',
+    './src/style/index.scss'
   ],
   output: {
     path: __dirname,
@@ -8,13 +12,19 @@ module.exports = {
     filename: 'bundle.js'
   },
   module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        presets: ['react', 'es2015', 'stage-1']
+    loaders: [
+      {
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015', 'stage-1']
+        }
+      },
+      {
+        test: /\.scss$/i,
+        loader: extractCSS.extract(['css','sass'])
       }
-    }]
+    ]
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
@@ -22,5 +32,8 @@ module.exports = {
   devServer: {
     historyApiFallback: true,
     contentBase: './'
-  }
+  },
+  plugins: [
+		extractCSS
+	]
 };
