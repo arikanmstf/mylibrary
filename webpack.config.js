cc7958e8ec1b4ea6c3b7518c9ec2043db8439d50
Mustafa Arikan <mustafa.arikan@monitise.com>
2017-06-02 10:23:02 +0300

    React and web pack updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-const argv = require('yargs').argv;
+const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
+const webpack = require("webpack");
+const argv = require("yargs").argv;
 
-let extractCSS = new ExtractTextPlugin('style.css');
-let UglifyJSP = new UglifyJSPlugin();
+
+const extractCSS = new ExtractTextPlugin("style.css");
 
 const isProd = argv.env === 'prod';
 const isDev = argv.env === 'dev';
@@ -11,40 +12,50 @@ const isDev = argv.env === 'dev';
 let plugins = [extractCSS];
 
 if (isProd) {
-  plugins.push(UglifyJSP)
+  plugins.push(
+    new UglifyJSPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    })
+  );
 }
 
 module.exports = {
   entry: [
-    './src/scripts/index.js',
-    './src/style/index.scss'
+    "./src/scripts/index.js",
+    "./src/style/index.scss"
   ],
   output: {
     path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
+    publicPath: "/",
+    filename: "bundle.js"
   },
   module: {
-    loaders: [
+    rules: [
       {
         exclude: /node_modules/,
-        loader: 'babel',
+        loader: "babel-loader",
         query: {
-          presets: ['react', 'es2015', 'stage-1']
+          presets: ["react", "es2015", "stage-1"]
         }
       },
       {
-        test: /\.scss$/i,
-        loader: extractCSS.extract(['css','sass'])
+        test: /\.scss$/,
+        use: extractCSS.extract({
+          fallback: "style-loader",
+          use: ["css-loader","sass-loader"]
+        })
       }
     ]
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: [".js", ".jsx"]
   },
   devServer: {
     historyApiFallback: true,
-    contentBase: './'
+    contentBase: "./"
   },
   plugins: plugins
 };
