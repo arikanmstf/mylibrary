2cca81ff35fb9b772f2a59d8168eaa49be966396
Mustafa Arikan <arikanmstf@gmail.com>
2017-06-03 19:02:28 +0300

    React router added

--- src/scripts/components/app.jsx
+++ src/scripts/components/app.jsx
@@ -3,11 +3,15 @@ import PublicationList from '../containers/publicationList';
 import LoginPage from '../containers/loginPage';
 import storage from '../common/storage';
 import NavbarHeader from '../containers/navbarHeader';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
 
 let s = new storage();
 let isLoggedIn = s.get("login_key");
 
-export default class App extends Component {
+class Home extends Component {
   constructor(props) {
     super(props);
 
@@ -30,14 +34,42 @@ export default class App extends Component {
 
   render() {
     return (
+          <div>
+            <NavbarHeader onSearchChange={ this.setSearchTitle } />
+            <PublicationList
+              search={this.state.search} />
+          </div>
+    );
+  }
+}
+
+class Admin extends Component {
+  render() {
+    return (
+      <div>
+          <h1>This is admin page.</h1>
+      </div>
+    );
+  }
+}
+
+export default class App extends Component {
+
+  render() {
+    return (
       <div className="main-container">
-        { isLoggedIn ? (<NavbarHeader onSearchChange={ this.setSearchTitle } />) : null }
-        { isLoggedIn ? (
-      	   <PublicationList
-             search={this.state.search} />
-        ) : (
+        { isLoggedIn ?
+          <div>
+            <Router>
+              <div>
+                <Route exact path="/" component={ Home }/>
+                <Route path="/admin" component={ Admin }/>
+              </div>
+            </Router>
+          </div>
+          :
           <LoginPage />
-        ) }
+        }
       </div>
     );
   }
--- src/scripts/containers/navbarHeader.jsx
+++ src/scripts/containers/navbarHeader.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import InputText from '../components/input/inputText';
 
 export default class NavbarHeader extends Component {
@@ -22,22 +23,24 @@ export default class NavbarHeader extends Component {
 
   render() {
     return (
-      <div className="navbar-header-container">
-        <nav className="navbar navbar-default">
-          <div className="container-fluid">
-            <div className="navbar-header">
-              <a className="navbar-brand">MyLibray</a>
+        <div className="navbar-header-container">
+          <nav className="navbar navbar-default">
+            <div className="container-fluid">
+              <div className="navbar-header">
+                <Link className="navbar-brand" to="/">MyLibray</Link>
+              </div>
+              <ul className="nav navbar-nav">
+                <li><Link to="/admin">Admin</Link></li>
+              </ul>
             </div>
-
+          </nav>
+          <div className="input-search-container">
+              <InputText
+                onChange={ this.handleSearchChange }
+                className="navbar-header-search"
+                placeholder="Type to search a book..."/>
           </div>
-        </nav>
-        <div className="input-search-container">
-            <InputText
-              onChange={ this.handleSearchChange }
-              className="navbar-header-search"
-              placeholder="Type to search a book..."/>
         </div>
-      </div>
     );
   }
 }
