de3d7589253e30c03fae45448ea391f9b9389c54
Mustafa Arikan <arikanmstf@gmail.com>
2017-06-04 20:59:01 +0300

    get lists of publication request removed.

--- src/scripts/common/config.js
+++ src/scripts/common/config.js
@@ -10,7 +10,6 @@ export const API = {
   getAllPublications: home + publications + "/get_all",
   getPublicationDetails: home + publications + "/get_one",
   getLogin: home + membership + "/get_login",
-  getListsOfPublication: home + lists + publications + "/get_all",
   getListById: home + lists  + "/get_one",
   getWriterDetails: home + writers  + "/get_one",
   getBookDetails: home + books  + "/get_one"
