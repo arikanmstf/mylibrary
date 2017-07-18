eac2ffdff1639394579635fcaafd4ac373c3b696
Mustafa Arikan <arikanmstf@gmail.com>
2017-07-18 22:33:20 +0300

    fix update publication details.

--- src/scripts/modules/publication-details/PublicationDetailsContainer.js
+++ src/scripts/modules/publication-details/PublicationDetailsContainer.js
@@ -1,5 +1,6 @@
 import { connect } from 'react-redux';
 
+import { updatePublicationDetailsList } from 'modules/admin/AdminActions';
 import PublicationDetailsComponent from './PublicationDetailsComponent';
 import { getPublicationDetails } from './PublicationDetailsActions';
 
