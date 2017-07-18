e077fa6b40d70e4d39feea1548c423a3f555eb23
Mustafa Arikan <arikanmstf@gmail.com>
2017-07-18 21:50:36 +0300

    Bugfix for home auth user route

--- src/scripts/app.jsx
+++ src/scripts/app.jsx
@@ -29,7 +29,7 @@ class App extends Component {
         <Router>
             { isLoggedIn ?
               <Switch>
-                <Route exact path="/" component={AuthUserRouter} />
+                <Route path="/" component={AuthUserRouter} />
                 <Route path="/admin" component={AuthAdminRouter} />
                 <Route path="*" component={NotFoundComponent} />
               </Switch>
--- src/scripts/routes/AuthUserRouter.jsx
+++ src/scripts/routes/AuthUserRouter.jsx
@@ -18,14 +18,14 @@ class AuthUserRouter extends Component {
     return (
     <div>
       <Route exact path={this.props.match.path} component={Home} />
-      <Route exact path={`${this.props.match.path}/pages/:pageNo`} component={Home} />
-      <Route exact path={`${this.props.match.path}/publications/publicationId`} component={PublicationDetails} />
-      <Route exact path={`${this.props.match.path}/writers/writerId`} component={WriterDetails} />
-      <Route exact path={`${this.props.match.path}/lists/listId`} component={ListDetails} />
-      <Route exact path={`${this.props.match.path}/books/bookId`} component={BookDetails} />
-      <Route exact path={`${this.props.match.path}/tags/tagId`} component={TagDetails} />
-      <Route exact path={`${this.props.match.path}/publishers/publisherId`} component={PublisherDetails} />
-      <Route exact path={`${this.props.match.path}/profile`} component={Profile} />
+      <Route exact path={`${this.props.match.path}pages/:pageNo`} component={Home} />
+      <Route exact path={`${this.props.match.path}publications/:publicationId`} component={PublicationDetails} />
+      <Route exact path={`${this.props.match.path}writers/:writerId`} component={WriterDetails} />
+      <Route exact path={`${this.props.match.path}lists/:listId`} component={ListDetails} />
+      <Route exact path={`${this.props.match.path}books/:bookId`} component={BookDetails} />
+      <Route exact path={`${this.props.match.path}tags/:tagId`} component={TagDetails} />
+      <Route exact path={`${this.props.match.path}publishers/:publisherId`} component={PublisherDetails} />
+      <Route exact path={`${this.props.match.path}profile`} component={Profile} />
 
       <Route exact path="/admin" component={AdminPage} />
     </div>);
