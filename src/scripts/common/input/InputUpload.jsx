efb1e1d0a0851d06c66703eee88f867c891da727
Mustafa Arikan <mustafa.arikan@monitise.com>
2017-07-27 14:12:12 +0300

    Show in progress message while upload in progress.

--- src/scripts/common/input/InputUpload.jsx
+++ src/scripts/common/input/InputUpload.jsx
@@ -4,13 +4,20 @@ import PropTypes from 'prop-types';
 import axios from 'axios';
 import { API } from 'common/Config';
 
+const UPLOAD_STATES = {
+  WAITING: 0,
+  IN_PROGRESS: 1,
+  DONE: 2,
+  FAILED: -1
+};
+
 class InputUpload extends Component {
 
   constructor(props) {
     super(props);
 
     this.state = {
-      file_uploaded: false
+      uploadState: UPLOAD_STATES.WAITING
     };
   }
 
@@ -23,12 +30,38 @@ class InputUpload extends Component {
       this.upload(`${API.uploadFile}?ext=${ext}`, file);
     }
   }
+
   upload(url, file) {
+    this.setState({ uploadState: UPLOAD_STATES.IN_PROGRESS });
     axios.put(url, file)
       .then((res) => {
-        this.setState({ file_uploaded: true });
+        this.setState({ uploadState: UPLOAD_STATES.DONE });
         this.props.onUpload(res.data);
+      })
+      .catch(() => {
+        this.setState({ uploadState: UPLOAD_STATES.FAILED });
       });
+  }
+
+  renderUploadState() {
+    let result = null;
+    switch (this.state.uploadState) {
+      case UPLOAD_STATES.WAITING:
+        result = <p>{this.props.title}</p>;
+        break;
+      case UPLOAD_STATES.IN_PROGRESS:
+        result = <p>{'Uploading...'}</p>;
+        break;
+      case UPLOAD_STATES.DONE:
+        result = <p>{'Done.'}</p>;
+        break;
+      case UPLOAD_STATES.FAILED:
+        result = <p>{'Failed.'}</p>;
+        break;
+      default:
+        result = null;
+    }
+    return result;
   }
 
 	render() {
@@ -40,10 +73,7 @@ class InputUpload extends Component {
             accept={this.props.accept}
             onDrop={(e, f) => this.onDropFile(e, f)}
           >
-            { this.state.file_uploaded ?
-              <p>Uploaded</p> :
-              <p>{this.props.title}</p>
-            }
+            {this.renderUploadState()}
           </Dropzone>
         </div>
       </div>
