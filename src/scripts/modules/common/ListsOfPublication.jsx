3b14ad93aaa25c1907d21e1bfc06bb31996aaec0
Mustafa Arikan <mustafa.arikan@monitise.com>
2017-08-17 17:35:42 +0300

    Unused files removed

--- src/scripts/modules/common/ListsOfPublication.jsx
+++ src/scripts/modules/common/ListsOfPublication.jsx
@@ -5,31 +5,31 @@ import config from 'config';
 
 class ListsOfPublication extends Component {
 
-	renderList() {
-		return this.props.lists.map((list) => {
-			return (
-				<div key={list.list_id}>
-					<Link to={`${config.homeUrl}lists/${list.list_id}`} >
-						<div className="list">
-								{list.title}
-						</div>
-					</Link>
-				</div>
-			);
-		});
-	}
+  renderList() {
+    return this.props.lists.map((list) => {
+      return (
+        <div key={list.list_id}>
+          <Link to={`${config.homeUrl}lists/${list.list_id}`} >
+            <div className="list">
+              {list.title}
+            </div>
+          </Link>
+        </div>
+      );
+    });
+  }
 
-	render() {
-		const lists = this.props.lists;
-		return lists ? (
-			<div className="lists-of-publication">
-						{ this.renderList() }
-			</div>
-		) : null;
-	}
+  render() {
+    const lists = this.props.lists;
+    return lists ? (
+      <div className="lists-of-publication">
+        { this.renderList() }
+      </div>
+    ) : null;
+  }
 }
 ListsOfPublication.propTypes = {
-	lists: PropTypes.arrayOf(Object).isRequired
+  lists: PropTypes.arrayOf(Object).isRequired
 };
 ListsOfPublication.defaultProps = {
   lists: [],
