8eb50e5f96245d0579623e4ed71a8598b526ab63
arikanmstf <arikanmstf@gmail.com>
2018-08-07 00:09:46 +0300

    Login tests updated

--- test/app/screens/login/Login.test.js
+++ test/app/screens/login/Login.test.js
@@ -7,11 +7,59 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import Login from 'screens/login/Login';
+import {
+  Text,
+  Screen,
+  TextField,
+  Button,
+  Row,
+  Col,
+  Image,
+  Form,
+} from 'ui';
+
+import {
+  Text as nText,
+  Screen as nScreen,
+  TextField as nTextField,
+  Button as nButton,
+  Row as nRow,
+  Col as nCol,
+  Image as nImage,
+  Form as nForm,
+} from 'ui/native';
 
 describe('test/app/screens/login/Login.test.js', () => {
-  const props = {};
+  it('Render-WEB', () => {
+    const props = {
+      Text,
+      Screen,
+      TextField,
+      Button,
+      Row,
+      Col,
+      Image,
+      Form,
+      handleSubmit: jest.fn(),
+    };
 
-  it('Render', () => {
+    const wrapper = shallow(<Login {...props} />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('Render-NATIVE', () => {
+    const props = {
+      Text: nText,
+      Screen: nScreen,
+      TextField: nTextField,
+      Button: nButton,
+      Row: nRow,
+      Col: nCol,
+      Image: nImage,
+      Form: nForm,
+      handleSubmit: jest.fn(),
+    };
+
     const wrapper = shallow(<Login {...props} />);
     expect(wrapper).toMatchSnapshot();
   });
