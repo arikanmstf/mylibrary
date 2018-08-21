7e7f206bca89d43fc413803c275ef092316c6f6b
arikanmstf <arikanmstf@gmail.com>
2018-08-21 19:11:23 +0300

    Tests updated.

--- test/app/ui/CardList/Native.test.js
+++ test/app/ui/CardList/Native.test.js
@@ -7,11 +7,13 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import CardList from 'ui/CardList/Native';
+import { createFakeStore } from 'helpers/connect/fakeStore';
 
 describe('test/app/ui/CardList/Native.test.js', () => {
   it('Render', () => {
     const props = {};
-    const wrapper = shallow(<CardList {...props} />);
+    const store = createFakeStore({ loader: { isVisible: false }, home: { cards: [] } });
+    const wrapper = shallow(<CardList {...props} store={store} />);
     expect(wrapper).toMatchSnapshot();
   });
 });
