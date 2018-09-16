4a9bd5510772457efc372e89b3ada988363ced7e
arikanmstf <arikanmstf@gmail.com>
2018-09-16 22:37:59 +0300

    Module creator script updated.

--- src/scripts/create-module/index.js
+++ src/scripts/create-module/index.js
@@ -1,7 +1,7 @@
 /**
  * Create Module - App file creator
  * @author arikanmstf
- * @version 1.0.0
+ * @version 1.1.0
  *
  */
 
@@ -11,217 +11,234 @@ const prompt = require('prompt');
 const fs = require('file-system');
 const p = require('path');
 
-function firstLetterUpper(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
-
-function firstLetterLower(str) {
-  return str.charAt(0).toLowerCase() + str.slice(1);
-}
-
-function getScreenTemplates() {
-  return {
-    screen: 'templates/screen/Screen.template',
-    actions: 'templates/screen/screenActions.template',
-    native: 'templates/screen/ScreenNativeContainer.template',
-    web: 'templates/screen/ScreenWebContainer.template',
-    types: 'templates/screen/ScreenTypes.template',
-    reducer: 'templates/screen/screenReducer.template',
-    index: 'templates/screen/index.template',
-    test: 'templates/screen/Screen.test.template',
+(function () {
+  const firstLetterUpper = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
   };
-}
-function getComponentTemplates() {
-  return {
-    native: 'templates/component/Native.template',
-    web: 'templates/component/Web.template',
-    types: 'templates/component/types.template',
-    test_native: 'templates/component/Native.test.template',
-    test_web: 'templates/component/Web.test.template',
+
+  const firstLetterLower = (str) => {
+    return str.charAt(0).toLowerCase() + str.slice(1);
   };
-}
 
-function screenSrcPath(screenName) {
-  return `src/app/screens/${screenName}`;
-}
-function componentSrcPath(componentName) {
-  return `src/app/ui/${componentName}`;
-}
-
-function screenPath(screenName) {
-  return `screens/${screenName}`;
-}
-function componentPath(screenName) {
-  return `ui/${screenName}`;
-}
-
-function testPath(screenName) {
-  return `test/app/screens/${screenName}`;
-}
-function testPathComponent(componentName) {
-  return `test/app/ui/${componentName}`;
-}
-
-function validateInput(screenName) {
-  const regex = /[a-zA-Z]/;
-  if (!regex.test(screenName)) {
-    throw new Error('Invalid screen name');
-  }
-}
-
-function readAndCreateFile(path, name, search, replace) {
-  fs.readFile(p.resolve(__dirname, path), 'utf8', (err, data) => {
-    if (err) {
-      throw new Error(err);
+  const validateInput = (name) => {
+    const regex = /[a-zA-Z]/;
+    if (!regex.test(name)) {
+      throw new Error('Invalid name');
     }
+  };
 
-    let newTemplate = data.toString();
-
-    if (search) {
-      if (Array.isArray(search)) {
-        search.forEach((s, index) => {
-          newTemplate = newTemplate.replace(new RegExp(`#{${search[index]}}`, 'g'), replace[index]);
-        });
-      } else {
-        newTemplate = data.toString().replace(new RegExp(`#{${search}}`, 'g'), replace);
+  const readAndCreateFile = (path, name, search, replace) => {
+    fs.readFile(p.resolve(__dirname, path), 'utf8', (err, data) => {
+      if (err) {
+        throw new Error(err);
       }
+
+      let newTemplate = data.toString();
+
+      if (search) {
+        if (Array.isArray(search)) {
+          search.forEach((s, index) => {
+            newTemplate = newTemplate.replace(new RegExp(`#{${search[index]}}`, 'g'), replace[index]);
+          });
+        } else {
+          newTemplate = data.toString().replace(new RegExp(`#{${search}}`, 'g'), replace);
+        }
+      }
+
+      fs.writeFile(name, newTemplate);
+    });
+  };
+
+  const PAGE_TYPE_SCREEN = 'PAGE_TYPE_SCREEN';
+  const PAGE_TYPE_COMPONENT = 'PAGE_TYPE_COMPONENT';
+  const PAGE_TYPE_MODULE = 'PAGE_TYPE_MODULE';
+
+  const getTemplates = () => {
+    let templates = {};
+
+    switch (this.pageType) {
+      case PAGE_TYPE_SCREEN:
+        templates = {
+          screen: 'templates/screen/Screen.template',
+          actions: 'templates/screen/screenActions.template',
+          native: 'templates/screen/ScreenNativeContainer.template',
+          web: 'templates/screen/ScreenWebContainer.template',
+          types: 'templates/screen/ScreenTypes.template',
+          reducer: 'templates/screen/screenReducer.template',
+          index: 'templates/screen/index.template',
+          test: 'templates/screen/Screen.test.template',
+        };
+        break;
+      case PAGE_TYPE_COMPONENT:
+        templates = {
+          native: 'templates/component/Native.template',
+          web: 'templates/component/Web.template',
+          types: 'templates/component/types.template',
+          test: 'templates/component/Web.test.template',
+          test_native: 'templates/component/Native.test.template',
+        };
+        break;
+      case PAGE_TYPE_MODULE:
+        templates = {
+          actions: 'templates/modules/actions.template',
+          reducer: 'templates/modules/reducer.template',
+          services: 'templates/modules/services.template',
+          types: 'templates/modules/types.template',
+        };
+        break;
+      default: console.error('page type not found');
     }
 
-    fs.writeFile(name, newTemplate);
-  });
-}
+    return templates;
+  };
 
-function createScreenName(path, screenName) {
-  return `${path}/${firstLetterUpper(screenName)}.js`;
-}
-function createComponentWebName(path) {
-  return `${path}/Web.js`;
-}
-function createComponentNativeName(path) {
-  return `${path}/Native.js`;
-}
-function createComponentTypesName(path) {
-  return `${path}/types.js`;
-}
-function createComponentWebTestName(path) {
-  return `${path}/Web.test.js`;
-}
-function createComponentNativeTestName(path) {
-  return `${path}/Native.test.js`;
-}
+  const getTestPath = () => {
+    let result = '';
 
-function createScreenActionName(path, screenName) {
-  return `${path}/${firstLetterLower(screenName)}Actions.js`;
-}
+    switch (this.pageType) {
+      case PAGE_TYPE_SCREEN: result = `test/app/screens/${this.name}`; break;
+      case PAGE_TYPE_COMPONENT: result = `test/app/ui/${firstLetterUpper(this.name)}`; break;
+      case PAGE_TYPE_MODULE: result = `test/app/modules/${firstLetterLower(this.name)}`; break;
+      default: console.error('page type not found');
+    }
+    return result;
+  };
 
-function createScreenNativeName(path, screenName) {
-  return `${path}/${firstLetterUpper(screenName)}NativeContainer.js`;
-}
+  const getPath = () => {
+    let result = '';
 
-function createScreenWebName(path, screenName) {
-  return `${path}/${firstLetterUpper(screenName)}WebContainer.js`;
-}
+    switch (this.pageType) {
+      case PAGE_TYPE_SCREEN: result = `screens/${this.name}`; break;
+      case PAGE_TYPE_COMPONENT: result = `ui/${firstLetterUpper(this.name)}`; break;
+      case PAGE_TYPE_MODULE: result = `modules/${firstLetterLower(this.name)}`; break;
+      default: console.error('page type not found');
+    }
+    return result;
+  };
 
-function createScreenTypeName(path, screenName) {
-  return `${path}/${firstLetterUpper(screenName)}Types.js`;
-}
+  const createName = (templateType) => {
+    let path = '';
+    let result = '';
+    const testP = getTestPath();
 
-function createScreenReducerName(path, screenName) {
-  return `${path}/${firstLetterLower(screenName)}Reducer.js`;
-}
+    switch (this.pageType) {
+      case PAGE_TYPE_SCREEN:
+        path = `src/app/screens/${this.name}`;
 
-function createIndexName(path) {
-  return `${path}/index.js`;
-}
+        switch (templateType) {
+          case 'screen': result = `${path}/${firstLetterUpper(this.name)}.js`; break;
+          case 'actions': result = `${path}/${firstLetterLower(this.name)}Actions.js`; break;
+          case 'native': result = `${path}/${firstLetterUpper(this.name)}NativeContainer.js`; break;
+          case 'web': result = `${path}/${firstLetterUpper(this.name)}WebContainer.js`; break;
+          case 'types': result = `${path}/${firstLetterUpper(this.name)}Types.js`; break;
+          case 'reducer': result = `${path}/${firstLetterLower(this.name)}Reducer.js`; break;
+          case 'index': result = `${path}/index.js`; break;
+          case 'test': result = `${testP}/${firstLetterUpper(this.name)}.test.js`; break;
+          default: console.error(`Screen template type not found: ${templateType}`);
+        }
+        break;
+      case PAGE_TYPE_COMPONENT:
+        path = `src/app/ui/${firstLetterUpper(this.name)}`;
 
-function createTestName(path, screenName) {
-  return `${path}/${firstLetterUpper(screenName)}.test.js`;
-}
+        switch (templateType) {
+          case 'native': result = `${path}/Native.js`; break;
+          case 'web': result = `${path}/Web.js`; break;
+          case 'types': result = `${path}/types.js`; break;
+          case 'test': result = `${testP}/Web.test.js`; break;
+          case 'test_native': result = `${testP}/Native.test.js`; break;
+          default: console.error(`Component template type not found: ${templateType}`);
+        }
+        break;
+      case PAGE_TYPE_MODULE:
+        path = `src/app/modules/${firstLetterLower(this.name)}`;
 
-function createComponentFiles(input) {
-  const componentName = firstLetterUpper(input);
-  const componentTemplates = getComponentTemplates();
-  const path = componentSrcPath(componentName);
-  const srcPath = componentPath(componentName);
-  const importFromWeb = `${srcPath}/Web`;
-  const importFromNative = `${srcPath}/Native`;
-  const testP = testPathComponent(componentName);
-  const testFileNameNative = createComponentNativeTestName(testP);
-  const testFileNameWeb = createComponentWebTestName(testP);
+        switch (templateType) {
+          case 'actions': result = `${path}/actions.js`; break;
+          case 'reducer': result = `${path}/reducer.js`; break;
+          case 'services': result = `${path}/services.js`; break;
+          case 'types': result = `${path}/types.js`; break;
+          case 'test': break;
+          default: console.error(`Module template type not found: ${templateType}`);
+        }
+        break;
+      default: console.error('page type not found');
+    }
 
-  readAndCreateFile(componentTemplates.native, createComponentNativeName(path), 'componentName', componentName);
-  readAndCreateFile(componentTemplates.web, createComponentWebName(path), 'componentName', componentName);
-  readAndCreateFile(componentTemplates.types, createComponentTypesName(path), 'componentName', componentName);
-  readAndCreateFile(
-    componentTemplates.test_native,
-    testFileNameNative,
-    ['componentName', 'testFileName', 'importFrom'],
-    [componentName, testFileNameNative, importFromNative]
-  );
-  readAndCreateFile(
-    componentTemplates.test_web,
-    testFileNameWeb,
-    ['componentName', 'testFileName', 'importFrom'],
-    [componentName, testFileNameWeb, importFromWeb]
-  );
-}
+    return result;
+  };
 
-function createScreenFiles(screenName) {
-  const className = firstLetterUpper(screenName);
-  const screenTemplates = getScreenTemplates();
-  const path = screenSrcPath(screenName);
-  const srcPath = screenPath(screenName);
+  const createFiles = () => {
+    const templates = getTemplates();
+    const keys = Object.keys(templates);
+    const className = firstLetterUpper(this.name);
+    const srcPath = getPath();
+    const testFileName = createName('test');
+    let importFrom = `${srcPath}/${className}`;
 
-  readAndCreateFile(screenTemplates.screen, createScreenName(path, screenName), 'className', className);
-  readAndCreateFile(screenTemplates.actions, createScreenActionName(path, screenName));
-  readAndCreateFile(screenTemplates.native, createScreenNativeName(path, screenName), ['className', 'screenName'], [className, screenName]);
-  readAndCreateFile(screenTemplates.web, createScreenWebName(path, screenName), ['className', 'screenName'], [className, screenName]);
-  readAndCreateFile(screenTemplates.reducer, createScreenReducerName(path, screenName), 'screenName', screenName);
-  readAndCreateFile(screenTemplates.types, createScreenTypeName(path, screenName), 'className', className);
-  readAndCreateFile(screenTemplates.index, createIndexName(path), 'className', className);
-  const testP = testPath(screenName);
-  const testFileName = createTestName(testP, screenName);
-  const importFrom = `${srcPath}/${className}`;
+    keys.forEach((key) => {
+      if (this.pageType === PAGE_TYPE_COMPONENT) {
+        switch (key) {
+          case 'test': importFrom = `${srcPath}/Web`; break;
+          case 'test_native': importFrom = `${srcPath}/Native`; break;
+          default: importFrom = '';
+        }
+      }
 
-  readAndCreateFile(
-    screenTemplates.test,
-    testFileName,
-    ['className', 'testFileName', 'importFrom'],
-    [className, testFileName, importFrom]
-  );
-}
+      readAndCreateFile(
+        templates[key],
+        createName(key),
+        ['className', 'screenName', 'testFileName', 'importFrom', 'componentName'],
+        [className, this.name, testFileName, importFrom, firstLetterUpper(this.name)]
+      );
+    });
+  };
 
-function createScreen() {
-  console.log('Enter screenName, example: bookDetail');
-  prompt.get(['screenName'], (err, result) => {
-    const screenName = firstLetterLower(result.screenName);
-    validateInput(screenName);
-    createScreenFiles(screenName);
-  });
-}
-function createComponent() {
-  console.log('Enter componentName, example: TextInput');
-  prompt.get(['componentName'], (err, result) => {
-    const componentName = firstLetterLower(result.componentName);
-    validateInput(componentName);
-    createComponentFiles(componentName);
-  });
-}
+  const create = () => {
+    let enterText;
 
-function main() {
-  console.log('Press "s" (Screen) or "c" (Component) :');
+    switch (this.pageType) {
+      case PAGE_TYPE_SCREEN:
+        enterText = 'Enter screen name, example: bookDetail';
+        break;
+      case PAGE_TYPE_COMPONENT:
+        enterText = 'Enter component name, example: TextInput';
+        break;
+      case PAGE_TYPE_MODULE:
+        enterText = 'Enter module name, example: card';
+        break;
+      default: console.error('page type not found');
+    }
+
+    console.log(enterText);
+    prompt.get(['name'], (err, result) => {
+      const name = firstLetterLower(result.name);
+      validateInput(name);
+      this.name = name;
+      createFiles();
+    });
+  };
+
+  console.log('Press "s" for Screen, "c"  for Component, "m" for Module :');
 
   prompt.get(['pageType'], (err, result) => {
-    const pageType = result && result.pageType ? result.pageType.replace(' ', '') : '';
+    let pageType = result && result.pageType ? result.pageType.replace(' ', '') : '';
 
     switch (pageType) {
-      case 's': createScreen(); break;
-      case 'c': createComponent(); break;
-      default: console.error('pageType not found');
+      case 's':
+        pageType = PAGE_TYPE_SCREEN;
+        break;
+      case 'c':
+        pageType = PAGE_TYPE_COMPONENT;
+        break;
+      case 'm':
+        pageType = PAGE_TYPE_MODULE;
+        break;
+      default: console.error('page type not found');
     }
-  });
-}
 
-main();
+    this.pageType = pageType;
+    create();
+  });
+}());
 
 /* eslint-enable no-console */
